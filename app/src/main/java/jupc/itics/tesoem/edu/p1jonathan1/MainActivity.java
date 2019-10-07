381a81566cb3dbafb88083825163b62e1261e019
github_jonathancardoso1895 <jupc18@hotmail.com>
2019-10-06 23:18:49 -0500

    Initial commit

---
+++ app/src/main/java/jupc/itics/tesoem/edu/p1jonathan1/MainActivity.java
@@ -0,0 +1,14 @@
+package jupc.itics.tesoem.edu.p1jonathan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
